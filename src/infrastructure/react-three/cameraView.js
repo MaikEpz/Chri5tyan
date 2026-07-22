0d7a2f0f61d0b1d5e03d936d968d052ddec716f8
MaikEpz <57566171+MaikEpz@users.noreply.github.com>
2026-07-22 00:24:17 -0500

    Adjust portrait camera yaw

--- src/infrastructure/react-three/cameraView.js
+++ src/infrastructure/react-three/cameraView.js
@@ -3,6 +3,7 @@ import { CAMERA } from "./sceneConfig.js";
 export const MOBILE_PORTRAIT_MAX_WIDTH = 767;
 export const MOBILE_PORTRAIT_DISTANCE_SCALE = 1.45;
 export const MOBILE_PORTRAIT_TARGET_RIGHT_OFFSET = 1.1;
+export const MOBILE_PORTRAIT_YAW_OFFSET = Math.PI / 180;
 
 export function getResponsiveCameraView(width, height) {
   const target = CAMERA.target.toArray();
@@ -25,15 +26,25 @@ export function getResponsiveCameraView(width, height) {
     0,
     forwardX / horizontalLength,
   ];
-  const portraitTarget = target.map((coordinate, index) => (
+  const portraitPosition = basePosition.map((coordinate, index) => (
+    target[index] + (coordinate - target[index]) * MOBILE_PORTRAIT_DISTANCE_SCALE
+  ));
+  const offsetTarget = target.map((coordinate, index) => (
     coordinate + right[index] * MOBILE_PORTRAIT_TARGET_RIGHT_OFFSET
   ));
+  const directionX = offsetTarget[0] - portraitPosition[0];
+  const directionZ = offsetTarget[2] - portraitPosition[2];
+  const yawCos = Math.cos(MOBILE_PORTRAIT_YAW_OFFSET);
+  const yawSin = Math.sin(MOBILE_PORTRAIT_YAW_OFFSET);
+  const portraitTarget = [
+    portraitPosition[0] + directionX * yawCos + directionZ * yawSin,
+    offsetTarget[1],
+    portraitPosition[2] - directionX * yawSin + directionZ * yawCos,
+  ];
 
   return {
     isMobilePortrait,
-    position: basePosition.map((coordinate, index) => (
-      target[index] + (coordinate - target[index]) * MOBILE_PORTRAIT_DISTANCE_SCALE
-    )),
+    position: portraitPosition,
     target: portraitTarget,
   };
 }
--- test/infrastructure/cameraView.test.js
+++ test/infrastructure/cameraView.test.js
@@ -3,6 +3,7 @@ import test from "node:test";
 import {
   MOBILE_PORTRAIT_DISTANCE_SCALE,
   MOBILE_PORTRAIT_TARGET_RIGHT_OFFSET,
+  MOBILE_PORTRAIT_YAW_OFFSET,
   getResponsiveCameraView,
 } from "../../src/infrastructure/react-three/cameraView.js";
 import { CAMERA } from "../../src/infrastructure/react-three/sceneConfig.js";
@@ -20,19 +21,37 @@ test("aleja la camara un 45 por ciento en celulares verticales", () => {
   });
 });
 
-test("orienta el encuadre inicial hacia el iPhone en movil vertical", () => {
+test("gira el encuadre vertical un grado a la izquierda", () => {
   const view = getResponsiveCameraView(390, 844);
   const basePosition = CAMERA.position.toArray();
   const baseTarget = CAMERA.target.toArray();
   const forwardX = baseTarget[0] - basePosition[0];
   const forwardZ = baseTarget[2] - basePosition[2];
   const horizontalLength = Math.hypot(forwardX, forwardZ);
-
-  assert.deepEqual(view.target, [
+  const portraitPosition = basePosition.map((coordinate, index) => (
+    baseTarget[index]
+      + (coordinate - baseTarget[index]) * MOBILE_PORTRAIT_DISTANCE_SCALE
+  ));
+  const offsetTarget = [
     baseTarget[0] + (-forwardZ / horizontalLength) * MOBILE_PORTRAIT_TARGET_RIGHT_OFFSET,
     baseTarget[1],
     baseTarget[2] + (forwardX / horizontalLength) * MOBILE_PORTRAIT_TARGET_RIGHT_OFFSET,
-  ]);
+  ];
+  const directionX = offsetTarget[0] - portraitPosition[0];
+  const directionZ = offsetTarget[2] - portraitPosition[2];
+
+  assert.ok(Math.abs(view.target[0] - (
+    portraitPosition[0]
+      + directionX * Math.cos(MOBILE_PORTRAIT_YAW_OFFSET)
+      + directionZ * Math.sin(MOBILE_PORTRAIT_YAW_OFFSET)
+  )) < Number.EPSILON);
+  assert.equal(view.target[1], offsetTarget[1]);
+  assert.ok(Math.abs(view.target[2] - (
+    portraitPosition[2]
+      - directionX * Math.sin(MOBILE_PORTRAIT_YAW_OFFSET)
+      + directionZ * Math.cos(MOBILE_PORTRAIT_YAW_OFFSET)
+  )) < Number.EPSILON);
+  assert.equal(MOBILE_PORTRAIT_YAW_OFFSET, Math.PI / 180);
 });
 
 test("aplica el encuadre vertical al iPhone de mayor ancho previsto", () => {
